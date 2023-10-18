0e36e5425d27c878c5bb66e73f4d566dc9a96f3e
rakshith <rakshit.s.gowda@gmail.com>
2023-10-18 19:43:15 +0530

    mux server router barebones

---
+++ cmd/web/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/CloudyKit/jet/v6"
+	"github.com/alexedwards/scs/v2"
+)
+
+type application struct {
+	appName string
+	server  server
+	debug   bool
+	errLog  *log.Logger
+	infoLog *log.Logger
+	view    *jet.Set
+	session *scs.SessionManager
+}
+
+type server struct {
+	port string
+	host string
+	url  string
+}
+
+func main() {
+	server := server{
+		host: "localhost",
+		port: "8009",
+		url:  "http://localhost:8009",
+	}
+	app := &application{
+		server:  server,
+		appName: "hackernews",
+		debug:   true,
+		infoLog: log.New(os.Stdout, "INFO\t", log.Ltime|log.Ldate|log.Lshortfile),
+		errLog:  log.New(os.Stdout, "INFO\t", log.Ltime|log.Ldate|log.Llongfile),
+	}
+
+	// JET template initalization
+	if app.debug {
+		app.view = jet.NewSet(jet.NewOSFileSystemLoader("./views"), jet.InDevelopmentMode())
+	} else {
+		app.view = jet.NewSet(jet.NewOSFileSystemLoader("./views"))
+	}
+
+	// Sessions initialization
+	app.session = scs.New()
+	app.session.Lifetime = 24 * time.Hour
+	app.session.Cookie.Persist = true
+	app.session.Cookie.Name = app.appName
+	app.session.Cookie.Domain = app.server.host
+	app.session.Cookie.SameSite = http.SameSiteStrictMode
+
+	// serve the App
+	if err := app.listenAndServe(); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("hello world!")
+}
---
+++ go.mod
@@ -0,0 +1,11 @@
+module techNEWS
+
+go 1.21.2
+
+require (
+	github.com/CloudyKit/jet/v6 v6.2.0
+	github.com/alexedwards/scs/v2 v2.5.1
+	github.com/go-chi/chi/v5 v5.0.10
+)
+
+require github.com/CloudyKit/fastprinter v0.0.0-20200109182630-33d98a066a53 // indirect
